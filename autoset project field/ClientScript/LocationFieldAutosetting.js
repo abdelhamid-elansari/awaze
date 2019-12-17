30bff80079250b41f3454855328c0f005d9c6dd2
mciccioli <mciccioli@deloitte.it>
2019-12-17 18:12:11 +0100

    filling of fields "location" and "department" during line creation

--- autoset project field/ClientScript/LocationFieldAutosetting.js
+++ autoset project field/ClientScript/LocationFieldAutosetting.js
@@ -1,165 +1,223 @@
 /**
  *@NApiVersion 2.0
- *@NScriptType UserEventScript
+ *@NScriptType ClientScript
  */
 
-define(['N/ui/serverWidget', 'N/record', 'N/runtime', 'N/log'], function(serverWidget, record, runtime, log) {
+define(['N/record', 'N/log'], function(record, log) {
 
-    /*
-            function beforeLoad(context) {
-
-
-                var myForm = context.form;
-
-                            var formObject=context.newRecord;
-                            var currentForm = recordObject.getValue('customform');
-                            var scriptObj = runtime.getCurrentScript();
-                            var currentFormId= scriptObj.getParameter({name: 'custscript_aw_templateid_locationautoset'});
-                            log.debug({
-                                title: 'Before Load current form ID',
-                                details: currentFormId
-                            });
-
-                            log.debug({
-                                title: 'Before Load custom form ID',
-                                details: currentForm
-                            });
-
-                          //  if (currentForm == currentFormId){
-                                //var form = context.form;
-                                log.debug({
-                                    title: 'Ingresso if FORM',
-                                    details: 'Entrato'
-                                });
-
-                            var sublist = myForm.getSublist({
-                                id : 'item'
-                            });
-                            var itemLocationField =  myForm.getSublist({id: 'item'}).getField({id: 'taxcode'});
-                        log.debug({
-                            title: 'Field to disable',
-                            details: itemLocationField
-                        });
-                            itemLocationField.isMandatory = false
-            log.debug({
-                title: 'Field after disabling',
-                details: itemLocationField
-            });
-                        //}
-                    }
-*/
-
-    function beforeSubmit(context) {
-
-        var recordObject=context.newRecord;
+    function fieldChanged(context) {
         log.debug({
-            title: 'print record',
-            details: recordObject
+            title: 'Check entering the function fieldChanged',
+            details: 'Entered'
         });
+        // Check on the sublists in wihich the field "location" is present
+        /*
+            if(context.sublistId == 'item'){
+                if(context.fieldId == 'location' ){
+                    var recordObject=context.currentRecord;
+                    log.debug({
+                        title: 'print record',
+                        details: recordObject
+                    });
+                    //Getting the value of the location and department from the header
+                    var locationHeader=recordObject.getValue('location');
+                    var departmentHeader=recordObject.getValue('department');
+                    log.debug({
+                        title: 'print of location in the header',
+                        details: locationHeader
+                    });
+                    log.debug({
+                        title: 'print of department in the header',
+                        details: departmentHeader
+                    });
 
-       // var currentForm = recordObject.getValue('customform');
-       // var scriptObj = runtime.getCurrentScript();
-       // var currentFormId= scriptObj.getParameter({name: 'custscript_aw_templateid_locationautoset'});
+                    log.debug({
+                        title: 'Check entering the function fieldChanged',
+                        details: 'Entered'
+                    });
 
-        //log.debug({
-        //    title: 'Current form',
-        //    details: currentForm
-        //});
-
-        //log.debug({
-        //    title: 'Current form ID',
-        //    details: currentFormId
-        //});
-
-       // if (currentForm == currentFormId){
-
-            log.debug({
-                title: 'if form',
-                details: 'Entrato'
-            });
-
-            //Getting the value of the location in the header
-            var locationHeader=recordObject.getValue('location');
-
-            //aggiorno le righe del tab item
-            var lineCount = recordObject.getLineCount('item');
-            log.debug({
-                title: 'n. lines in the tab Items',
-                details: lineCount
-            });
-
-            for (var j = 0; j < lineCount; j++){
-
-                //Getting the selected valued for the custom field "PROJECT"
-                var locationLine = recordObject.getSublistValue({
-                    sublistId: 'item',
-                    fieldId: 'location',
-                    line: j
-                });
-
-                if(locationLine==''){
-
-                    var locationField = recordObject.setSublistValue({
+                    //filling the field "location"
+                    var locationLine = context.currentRecord.getCurrentSublistValue({
                         sublistId: 'item',
                         fieldId: 'location',
-                        value: locationHeader,
-                        line: j
+                        line: context.line
                     });
-
-                    /*
                     log.debug({
-                        title: 'Value of location in the line',
-                        details: locationField.description
+                        title: 'Sublist',
+                        details: context.sublistId
                     });
-
-                     */
-
-                }else{
                     log.debug({
-                        title: 'Filling of location in the line',
-                        details: 'The field location is already filled in the line'
+                        title: 'Location in the line',
+                        details: locationLine
                     });
+                    if (locationLine == '') {
+                        context.currentRecord.setCurrentSublistValue({
+                            sublistId: 'item',
+                            fieldId: 'location',
+                            value: locationHeader
+                        });
+                    }
+                }else if(context.fieldId == 'department'){
+                    var recordObject=context.currentRecord;
+                    log.debug({
+                        title: 'print record',
+                        details: recordObject
+                    });
+                    //Getting the value of the location and department from the header
+                    var locationHeader=recordObject.getValue('location');
+                    var departmentHeader=recordObject.getValue('department');
+                    log.debug({
+                        title: 'print of location in the header',
+                        details: locationHeader
+                    });
+                    log.debug({
+                        title: 'print of department in the header',
+                        details: departmentHeader
+                    });
+                        //filling the field "department"
+                        var departmentLine = context.currentRecord.getCurrentSublistValue({
+                            sublistId: 'item',
+                            fieldId: 'department',
+                            line: context.line
+                        });
+                        log.debug({
+                            title: 'Sublist',
+                            details: context.sublistId
+                        });
+                        log.debug({
+                            title: 'department in the line',
+                            details: departmentLine
+                        });
+                        if (departmentLine==''){
+                            context.currentRecord.setCurrentSublistValue({
+                                sublistId: 'item',
+                                fieldId: 'department',
+                                value: departmentHeader
+                            });
+                        }
                 }
+
             }
 
-            //aggiorno le righe del tab expenses
-            var lineCount = recordObject.getLineCount('expense');
-            log.debug({
-                title: 'n. lines in the tab Expenses',
-                details: lineCount
-            });
-
-            for (var j = 0; j < lineCount; j++){
-
-                //Getting the selected valued for the custom field "PROJECT"
-                var locationLine = recordObject.getSublistValue({
-                    sublistId: 'expense',
-                    fieldId: 'location',
-                    line: j
+         */
+            if(context.sublistId === 'item'){
+                log.debug({
+                    title: 'Check entering into expenses',
+                    details: 'Entered'
+                });
+                log.debug({
+                    title: 'Name of the field',
+                    details: context.fieldId
                 });
 
-                if(locationLine==''){
-
-                    var CustomProjectField = recordObject.setSublistValue({
-                        sublistId: 'expense',
+                var recordObject=context.currentRecord;
+                log.debug({
+                    title: 'print record',
+                    details: recordObject
+                });
+                //Getting the value of the location and department from the header
+                var locationHeader=recordObject.getValue('location');
+                var departmentHeader=recordObject.getValue('department');
+                log.debug({
+                    title: 'print of location in the header',
+                    details: locationHeader
+                });
+                log.debug({
+                    title: 'print of department in the header',
+                    details: departmentHeader
+                });
+                var itemLine = context.currentRecord.getCurrentSublistValue({
+                    sublistId: 'item',
+                    fieldId: 'item',
+                    line: context.line
+                });
+                var locationLine = context.currentRecord.getCurrentSublistValue({
+                    sublistId: 'item',
+                    fieldId: 'location',
+                    line: context.line
+                });
+                if (itemLine !== '' && locationLine === '') {
+                    context.currentRecord.setCurrentSublistValue({
+                        sublistId: 'item',
                         fieldId: 'location',
-                        value: locationHeader,
-                        line: j
-                    });
-
-                }else{
-                    log.debug({
-                        title: 'Filling of location in the line',
-                        details: 'The field location is already filled in the line'
+                        value: locationHeader
                     });
                 }
+                var departmentLine = context.currentRecord.getCurrentSublistValue({
+                    sublistId: 'item',
+                    fieldId: 'department',
+                    line: context.line
+                });
+                if (itemLine !== '' && departmentLine === '') {
+                    context.currentRecord.setCurrentSublistValue({
+                        sublistId: 'item',
+                        fieldId: 'department',
+                        value: departmentHeader
+                    });
+                }
+
             }
-       // }
+            if(context.sublistId === 'expense'){
+                log.debug({
+                    title: 'Check entering into expenses',
+                    details: 'Entered'
+                });
+                log.debug({
+                    title: 'Name of the field',
+                    details: context.fieldId
+                });
+
+                var recordObject=context.currentRecord;
+                log.debug({
+                    title: 'print record',
+                    details: recordObject
+                });
+                //Getting the value of the location and department from the header
+                var locationHeader=recordObject.getValue('location');
+                var departmentHeader=recordObject.getValue('department');
+                log.debug({
+                    title: 'print of location in the header',
+                    details: locationHeader
+                });
+                log.debug({
+                    title: 'print of department in the header',
+                    details: departmentHeader
+                });
+                var accountLine = context.currentRecord.getCurrentSublistValue({
+                    sublistId: 'expense',
+                    fieldId: 'account',
+                    line: context.line
+                });
+                var locationLine = context.currentRecord.getCurrentSublistValue({
+                    sublistId: 'expense',
+                    fieldId: 'location',
+                    line: context.line
+                });
+                if (accountLine !== '' && locationLine === '') {
+                    context.currentRecord.setCurrentSublistValue({
+                        sublistId: 'expense',
+                        fieldId: 'location',
+                        value: locationHeader
+                    });
+                }
+                var departmentLine = context.currentRecord.getCurrentSublistValue({
+                    sublistId: 'expense',
+                    fieldId: 'department',
+                    line: context.line
+                });
+                if (accountLine !== '' && departmentLine === '') {
+                    context.currentRecord.setCurrentSublistValue({
+                        sublistId: 'expense',
+                        fieldId: 'department',
+                        value: departmentHeader
+                    });
+                }
+
+            }
     }
 
     return {
-        //beforeLoad: beforeLoad,
-        beforeSubmit: beforeSubmit
+        fieldChanged: fieldChanged
     };
 });
 
